95e763918bcb8beb3423ffe6f22996a3e58eed35
zshnrg <ghosanirozan1@gmail.com>
2025-03-07 17:22:57 +0700

    feat: add input error handling

--- one-kanban/src/app/(frontend)/auth/_components/login.tsx
+++ one-kanban/src/app/(frontend)/auth/_components/login.tsx
@@ -12,9 +12,39 @@ export default function Login() {
     const [ isVisible, setIsVisible ] = useState(false);
     const router = useRouter();
 
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    });
+
+    const [errors, setErrors] = useState<{ 
+        email?: string; 
+        password?: string 
+    }>({});
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    }
+
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log('submit');
+        
+        if (!formData.email) {
+            setErrors({ ...errors, email: "Email is required" });
+        } else {
+            delete errors.email;
+        }
+
+        if (!formData.password) {
+            setErrors({ ...errors, password: "Password is required" });
+        } else {
+            delete errors.password;
+        }
+
+        if (Object.keys(errors).length === 0) {
+            console.log(formData);
+        }
     }
 
     return (
@@ -23,15 +53,28 @@ export default function Login() {
             onSubmit={handleSubmit}
         >
             <div className="flex flex-col gap-4">
-                <Input type="email" id="email" name="email" placeholder="Email" />
-                <Input 
-                    type={ isVisible ? "text" : "password" }
-                    id="password"
-                    name="password"
-                    placeholder="Password"
-                    leadingIcon={ isVisible ? <LuEyeClosed /> : <LuEye /> } 
-                    onLeadingIconClick={() => setIsVisible(!isVisible)}
-                />
+                <div>
+                    <Input 
+                        type="email" 
+                        id="email" 
+                        name="email" 
+                        placeholder="Email"
+                        onChange={handleChange}
+                    />
+                    {errors.email && <p className="ml-4 mt-2 text-red-500 text-sm">{errors.email}</p>}
+                </div>
+                <div>
+                    <Input 
+                        type={ isVisible ? "text" : "password" }
+                        id="password"
+                        name="password"
+                        placeholder="Password"
+                        leadingIcon={ isVisible ? <LuEyeClosed /> : <LuEye /> } 
+                        onLeadingIconClick={() => setIsVisible(!isVisible)}
+                        onChange={handleChange}
+                    />
+                    {errors.password && <p className="ml-4 mt-2 text-red-500 text-sm">{errors.password}</p>}
+                </div>
             </div>
             <div className="flex gap-4">
                 <Button 
