86f7af89d96be4eb21400f5e46f4652a4b0b509b
zshnrg <ghosanirozan1@gmail.com>
2025-03-08 15:59:57 +0700

    feat: adding side bar hide and show animation - add motion package

--- ban-kanban/src/app/(users)/layout.tsx
+++ ban-kanban/src/app/(users)/layout.tsx
@@ -1,14 +1,19 @@
 'use client'
+
 import Navbar from "@/components/layout/navbar";
+import SideBar from "@/components/layout/sidebar";
 
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
 
     return (
         <div className="flex flex-col min-h-full">
             <Navbar />
-            <main className="flex-1">
-                {children}
-            </main>
+            <div className="flex w-full h-full mt-14 lg:mt-16">
+                <SideBar />
+                <main className="flex-1">
+                    {children}
+                </main>
+            </div>
         </div>
     )
 }
---
+++ ban-kanban/src/lib/types/project.ts
@@ -0,0 +1,81 @@
+export interface Project {
+    project_id: number;
+    project_name: string;
+    project_description: string;
+    project_status: string;
+    project_created_at: string;
+}
+
+
+export const projectDummy: Project[] = [
+    {
+        project_id: 1,
+        project_name: "Project 1",
+        project_description: "Project 1 Description",
+        project_status: "Active",
+        project_created_at: "2022-01-01",
+    },
+    {
+        project_id: 2,
+        project_name: "Project 2",
+        project_description: "Project 2 Description",
+        project_status: "Active",
+        project_created_at: "2022-01-01",
+    },
+    {
+        project_id: 3,
+        project_name: "Project 3",
+        project_description: "Project 3 Description",
+        project_status: "Active",
+        project_created_at: "2022-01-01",
+    },
+    {
+        project_id: 4,
+        project_name: "Project 4",
+        project_description: "Project 4 Description",
+        project_status: "Active",
+        project_created_at: "2022-01-01",
+    },
+    {
+        project_id: 5,
+        project_name: "Project 5",
+        project_description: "Project 5 Description",
+        project_status: "Active",
+        project_created_at: "2022-01-01",
+    },
+    {
+        project_id: 6,
+        project_name: "Project 6",
+        project_description: "Project 6 Description",
+        project_status: "Active",
+        project_created_at: "2022-01-01",
+    },
+    {
+        project_id: 7,
+        project_name: "Project 7",
+        project_description: "Project 7 Description",
+        project_status: "Active",
+        project_created_at: "2022-01-01",
+    },
+    {
+        project_id: 8,
+        project_name: "Project 8",
+        project_description: "Project 8 Description",
+        project_status: "Active",
+        project_created_at: "2022-01-01",
+    },
+    {
+        project_id: 9,
+        project_name: "Project 9",
+        project_description: "Project 9 Description",
+        project_status: "Active",
+        project_created_at: "2022-01-01",
+    },
+    {
+        project_id: 10,
+        project_name: "Project 10",
+        project_description: "Project 10 Description",
+        project_status: "Active",
+        project_created_at: "2022-01-01",
+    }
+];
