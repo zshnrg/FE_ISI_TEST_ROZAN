d705962465dca78283a6e7c8568afe60bcd43394
zshnrg <ghosanirozan1@gmail.com>
2025-03-08 13:18:03 +0700

    feat: profil pic generator

---
+++ ban-kanban/src/components/ui/profile.tsx
@@ -0,0 +1,21 @@
+export function UserProfileImage({
+    full_name,
+    bgColor,
+    size
+} : {
+    full_name: string;
+    bgColor: string;
+    size: number;
+}) {
+    const names = full_name.split(" ").slice(0, 2);
+    const initials = names.map((name) => name[0].toUpperCase());
+    // Component to display user profile image with initials
+    return (
+        <div 
+            className={`flex items-center justify-center rounded-full text-white`}
+            style={{ backgroundColor: bgColor, width: size, height: size, fontSize: size / 2 }}
+        >
+            <span className="text-xl font-semibold">{initials}</span>
+        </div>
+    );
+}
